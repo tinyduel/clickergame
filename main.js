ee1ef37a91a292090479d0077d77323019cb1b7f
tinyduel <46302335+tinyduel@users.noreply.github.com>
2019-01-31 13:34:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function buyKeyboard() {
   if (cookies >= keyboardCost) {
     keyboard = keyboard + 1
     cookies = cookies - keyboardCost
+    keyboardCost = keyboardCost + keyboard
     document.getElementById("cookies").innerHTML = cookies;
     document.getElementById("keyboard").innerHTML = keyboard;
     document.getElementById("keyboardCost").innerHTML = keyboardCost;
