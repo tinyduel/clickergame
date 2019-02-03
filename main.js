c05dce14fe6d31df07b41a4ad72dcfe97f0fed42
tinyduel <46302335+tinyduel@users.noreply.github.com>
2019-02-02 20:00:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ function stocksUpdate() {
 	//	document.getElementById("stocks").innerHTML = stocks;
 		document.getElementById("stockPrice").innerHTML = stockPrice;
 	};
+	document.getElementById("stocksCost").innerHTML = stockPrice + 5;
 };
 window.setInterval(function(){
 	
